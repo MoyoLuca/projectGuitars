02fe511282e95a9efc573d8b5acc07a55a5f67d7
mluevano <mike_luev@outlook.com>
2023-09-15 15:17:58 -0600

    Proyecto Base

---
+++ net/Guitar_Angular_Web/Common/Business.Common/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DataModels.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Common.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Guitars> Guitars { get; set; }
+
+        // Otras entidades y configuraciones aquí
+    }
+}
---
+++ net/Guitar_Angular_Web/Common/DataModels/ViewModels/UserInfo.cs
@@ -0,0 +1,28 @@
+﻿using DataModels.DbModels;
+
+namespace DataModels.ViewModels
+{
+    public class UserInfo
+    {
+        public UserInfo()
+        {
+
+        }
+
+        public UserInfo(Users user)
+        {
+            Id = user.Id;
+            UserName = user.UserName;
+            Email = user.EmailAddress;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+        }
+
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+
+    }
+}
